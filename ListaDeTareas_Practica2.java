cacaa5176880bce2ef82c4a17394ac36e6fd7fb6
mixuxi <143725801+mixuxi@users.noreply.github.com>
2023-09-05 08:52:13 -0600

    Add files via upload

---
+++ ListaDeTareas_Practica2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package listadetareas;
+
+/**
+ *
+ * @author labcca
+ */
+public class ListaDeTareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Tarea1 = "Ejercicos y practica 1 de calculo diferencial";
+        String Tarea2 = "Exposición de taller de etica";
+        String Tarea3 = "Practica 1 de fundamentos de programacion";
+        String Tarea4 = "Exposicion linea del tiempo de Introduccion a las TICs";
+        
+        System.out.println("Tarea 1: " + Tarea1);
+        System.out.println("Tarea 2: " + Tarea2);
+        System.out.println("Tarea 3: " + Tarea3);
+        System.out.println("Tarea 4: " + Tarea4);
+    }
+    
+}
