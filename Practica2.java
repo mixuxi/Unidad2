e09c7c9b2263b54e003262335bd7850b50d1e9e2
mixuxi <143725801+mixuxi@users.noreply.github.com>
2023-09-07 09:56:45 -0500

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+public static void main(String[] args) {
+    int nombre;
+    System.out.println("mi nombre es:"+"daniela turin mixuxi");
+    int base=45;
+    int altura=4;
+    int arearectangulo=base*altura;
+    double baseA=5.8;
+    double alturaB=4;
+    double arearectangulodecimal=baseA*alturaB;
+    System.out.println("el area rectangulo decimal es "+arearectangulodecimal);
+          
+}
+   
+    
+}
